89c4288e50a8d103d6fc6dafa818198a1a30adb5
iscarion86 <malmosb@gmail.com>
2025-12-08 19:40:09 +0100

    Egy új branch, hogy ezt a lehetőséget is bemutassuk

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World - "app" by NLC3U6'
-
+#Feature branch demó - ide lehetne valami fancy új funkciót tenni
 app.run(host='0.0.0.0', port=8080)
